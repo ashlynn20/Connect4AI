7217e577296d8849cb765e795cbf193e04452d4d
Ashly Hervey <ahervey@colostate.edu>
2024-04-01 14:40:24 -0600

    Updated

--- connect4.py
+++ connect4.py
@@ -4,61 +4,50 @@ who_won = 'no one'
 
 def connect_4():
     board = [[0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0]]
-    count = -1
 
     for i in board:
         print(i)
 
     while(win(board) != True):
-        count += 1
-        if count % 2 == 0:
-            print('Enter column to add piece')
-            position = int(input())
-            board = add_piece_player_one(position, board)
-            for i in board:
-                print(i)
-        else:
-            print('Enter column to add piece')
-            position = int(input())
-            board = add_piece_player_two(position, board)
-            for i in board:
-                print(i)
+        decision(board, 1)
+        if(win(board) == True):
+            break
+        decision(board, -1)
     
     print(who_won, 'won!')
 
-def add_piece_player_one(position, board):
-    count = len(board) - 1
-    while count > -1:
-        if board[count][position] == 0:
-            board[count][position] = 1
-            break
-        count -= 1
-    return board
+def decision(board, num):
+    print('Enter column to add piece')
+    position = int(input())
+    board = add_piece(position, board, num)
+    for i in board:
+        print(i)
 
-def add_piece_player_two(position, board):
+def add_piece(position, board, num):
     count = len(board) - 1
     while count > -1:
         if board[count][position] == 0:
-            board[count][position] = -1
+            board[count][position] = num
             break
         count -= 1
     return board
 
 def win(board):
-    global who_won
     #horizontal win
-    for x in board:
-        player_pos = any([1, 1, 1, 1] == list(x) for x in zip(*[x[i:] for i in range(len([1, 1, 1, 1]))]))
-        if player_pos == True:
-            who_won = 'Player one'
-            return True
-        player_neg = any([-1, -1, -1, -1] == list(x) for x in zip(*[x[i:] for i in range(len([-1, -1, -1, -1]))]))
-        if player_neg == True:
-            who_won = 'Player two'
-            return True
+    if True == horizontal_vertical(board):
+        return True
+
     #vertical win
     board = np.array(board)
     board = board.T
+    if True == horizontal_vertical(board):
+        return True
+
+    #diagonal win
+    return False
+
+def horizontal_vertical(board):
+    global who_won
     for x in board:
         player_pos = any([1, 1, 1, 1] == list(x) for x in zip(*[x[i:] for i in range(len([1, 1, 1, 1]))]))
         if player_pos == True:
@@ -68,8 +57,6 @@ def win(board):
         if player_neg == True:
             who_won = 'Player two'
             return True
-        
-    #diagonal win
     return False
 
 
