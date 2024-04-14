ed24be3f8712bb490baaa6fb5c02d12533820bc8
Livia Lane <liv.lane0524@gmail.com>
2024-04-14 16:01:04 -0600

    addding variables

--- ai.py
+++ ai.py
@@ -4,5 +4,17 @@ action = np.array([0,1,2,3,4,5,6])
 state = np.zeros((6,7))
 qTable = np.zeros((7,42))
 
+alpha = 0 ##learning rate
+gamma = 0 ## discount factor
+epsilon = 0 #exploration vs exploitation
+num_games = 0
+
+
+def qLearning_equation():
+    return
+
+def epsilon_greedy_algorithm():
+    return
+
 print(action)
 print(state)
