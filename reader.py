d496aca837e28f14ec7bc74daf0c7204d23e2afe
Ashly Hervey <ahervey@colostate.edu>
2024-04-29 17:05:58 -0600

    halp

--- reader.py
+++ reader.py
@@ -1,19 +1,17 @@
 import numpy as np
 
-def file_reader():
-    data = np.loadtxt('con.data', delimiter=',', dtype=str)
+def file_reader(pattern):
+    data = np.loadtxt('connectData.data', delimiter=',', dtype=str)
     features = data[:, :-1]  
     labels = data[:, -1] 
     print("Features:")
     print(features)
     print("Labels:")
     print(labels)
-    win_count = 0
-
-    for i in range(len(labels)):
-        if labels[i] == 'win':
-            win_count += 1
     
-    print(win_count)
+    for i in range(len(features)):
+        if np.array_equal(features[i], pattern):
+            #could throw in condition here that determines f it's a win and rewards based on it
+            print("found")
 
 file_reader()
